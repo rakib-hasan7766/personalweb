26c924ddf3f55393c33cf6d2164a41c8f155bc96
rakib-hasan7766 <service.infobg@gmail.com>
2026-05-19 06:14:04 +0600

    Update db.php

--- db.php
+++ db.php
@@ -1,18 +1,14 @@
 <?php
-// Render থেকে পাওয়া আপনার External Database URL টি এখানে বসাবেন
-$db_url = "postgres://your_user:your_password@your_host.render.com/your_dbname";
+$host = "dpg-d85q0mnavr4c73d62fog-a.oregon-postgres.render.com"; 
+$user = "perwebb"; 
+$password = "NGeh1PNCgSo3e36xH5oTrQqUBFPXMsx3"; 
+$dbname = "perweb"; 
+$port = "5432"; 
 
-$db_config = parse_url($db_url);
-$host = $db_config['dpg-d85q0mnavr4c73d62fog-a'];
-$user = $db_config['perwebb'];
-$password = $db_config['NGeh1PNCgSo3e36xH5oTrQqUBFPXMsx3'];
-$dbname = ltrim($db_config['perweb'], '/');
-$port = $db_config['port'];
-
-$connection_string = "host=$host port=$port dbname=$dbname user=$user password=$password";
+$connection_string = "host=$host port=$port dbname=$dbname user=$user password=$password sslmode=require";
 $conn = pg_connect($connection_string);
 
 if (!$conn) {
-    die("সার্ভার কানেকশন সাময়িকভাবে ব্যাহত হচ্ছে।");
+    die("ডাটাবেজ কানেকশন সফল হয়নি।");
 }
 ?>
